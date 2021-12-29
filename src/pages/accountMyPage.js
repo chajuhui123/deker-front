e180635af484c488abd49feb1f99d91d6317ddbc
yrkwon <moonctp24@gmail.com>
2021-12-29 12:26:49 +0900

    modify CSS file name to 'module.css'

--- src/pages/accountMyPage.js
+++ src/pages/accountMyPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./accountMyPage.css";
+import "./accountMyPage.module.css";
 
 function AccountMyPage(props) {
   return (
