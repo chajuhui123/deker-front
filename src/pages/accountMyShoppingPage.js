834742fff97b8b389a56bbebdd6345e213cfdf1c
yrkwon <moonctp24@gmail.com>
2021-12-30 19:54:41 +0900

    Create Account My Shopping Page

---
+++ src/pages/accountMyShoppingPage.js
@@ -0,0 +1,11 @@
+import AccountMyShopping from "../components/account/accountMyShopping";
+
+const AccountMyShoppingPage = () => {
+  return (
+    <div>
+      <AccountMyShopping />
+    </div>
+  );
+};
+
+export default AccountMyShoppingPage;
