64139954d22ea12d5e3600663615cdd33e26a159
Ju <jjuhee0913@gmail.com>
2022-01-22 20:51:01 +0900

    feat : store 상품 상세보기 페이지 레이아웃 구현

--- src/components/shop/productDetail/productRecommendBox.js
+++ src/components/shop/productDetail/productRecommendBox.js
@@ -1,7 +1,20 @@
 import React from "react";
+import classes from "./box.module.css";
+import ProductRecommendItem from "./productRecommendItem";
 
 function ProductRecommendBox() {
-  return <div></div>;
+  return (
+    <div className={classes.boxDiv}>
+      <h1 className={classes.contentTitle}>함께하면 좋은 상품</h1>
+      <hr />
+      <div style={{ display: "flex" }}>
+        <ProductRecommendItem />
+        <ProductRecommendItem />
+        <ProductRecommendItem />
+        <ProductRecommendItem />
+      </div>
+    </div>
+  );
 }
 
 export default ProductRecommendBox;
--- src/components/shop/productDetail/productReviewItem.js
+++ src/components/shop/productDetail/productReviewItem.js
@@ -1,7 +1,38 @@
 import React from "react";
+import classes from "./productReviewItem.module.css";
+import noUserImg from "img/noUserImg.png";
 
 function ProductReviewItem() {
-  return <div></div>;
+  const userImg = "";
+  const userId = "ID1234";
+  const productName = "상품명 예시";
+  const productColorOption = "옵션1234";
+  const productOption = "옵션1234";
+  const reviewDate = "2022-01-22";
+  const rating = "4";
+  const productReview =
+    "후기글을 적는 곳입니다. 동대문 엽기떡볶이 본점은 동대문에 있다.";
+  return (
+    <div className={classes.productReviewDiv}>
+      <img
+        className={classes.userImg}
+        src={userImg || noUserImg}
+        alt="유저이미지"
+      />
+      <div>
+        <div className={classes.reviewWriterInfoDiv}>
+          <p>{userId}</p>
+          <p>{reviewDate}</p>
+        </div>
+        <p>{productName}</p>
+        <div className={classes.productOptionDiv}>
+          <p>{productColorOption}</p>
+          <p>{productOption}</p>
+        </div>
+        <p>{productReview}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProductReviewItem;
