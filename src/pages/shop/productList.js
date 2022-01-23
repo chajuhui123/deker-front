322e3de367951b77eb479fda7ea38d66922365e2
yrkwon <moonctp24@gmail.com>
2022-01-23 14:33:29 +0900

    outer product func

--- src/pages/shop/productList.js
+++ src/pages/shop/productList.js
@@ -2,21 +2,18 @@ import ProductItem from "./productItem";
 import classes from "./productList.module.css";
 
 function ProductList(props) {
-  return (
-    <ul>
-      {props.products.map((product) => (
-        <li className={classes.productList}>
-          <ProductItem
-            key={product.id}
-            id={product.id}
-            productImg={product.productImg}
-            productNm={product.productNm}
-            productPrice={product.productPrice}
-          />
-        </li>
-      ))}
-    </ul>
-  );
+  const productList = props.products.map((product) => (
+    <li className={classes.productList}>
+      <ProductItem
+        key={product.id}
+        id={product.id}
+        productImg={product.productImg}
+        productNm={product.productNm}
+        productPrice={product.productPrice}
+      />
+    </li>
+  ));
+  return <ul>{productList}</ul>;
 }
 
 export default ProductList;
