ec764297dc2f14892e91f55c6fa648f9c34eefd9
yrkwon <moonctp24@gmail.com>
2022-03-03 22:22:01 +0900

    상품등록 개발중

--- src/pages/shop/productItem.js
+++ src/pages/shop/productItem.js
@@ -1,13 +1,34 @@
 import { Link } from "react-router-dom";
 import classes from "./productItem.module.css";
+import classes2 from "./productItem2.module.css";
 import noImg from "img/noImg.png";
 import { BASE_URL } from "module/common-module";
+import CommBtn from "components/common/commBtn";
+// import { useState } from "react";
 
 function ProductItem(props) {
   var productId = props.mktProductId;
+  // const [productClick, setProductClick] = useState(false);
+
+  var departure = props.departure;
+  var cssClass = classes;
+  // storeMain 페이지에서 product List를 조회할 때 사용할 css 파일
+  if (departure === "storeMain") {
+    cssClass = classes;
+  }
+  // productSalesLink 페이지에서 product List를 조회할 때 사용할 css 파일
+  else if (departure === "productSalesLink") {
+    cssClass = classes2;
+  }
+
+  const productClickBtnHandler = () => {
+    // setProductClick(true);
+    // props.productClickHandler(productClick);
+  };
+
   return (
     <div>
-      <div>
+      <div className={cssClass.productArea}>
         <Link
           to={{
             pathname: `/market/detail/${productId}`,
@@ -15,15 +36,25 @@ function ProductItem(props) {
           }}
         >
           <img
-            className={classes.productImg}
+            className={cssClass.productImg}
             alt={props.productNm}
             src={`${BASE_URL}${props.productImg}` || noImg}
+            // src={props.productImg}
           />
         </Link>
-        <div className={classes.productInfo}>
+        <div className={cssClass.productInfo}>
           <div>{props.productNm}</div>
-          <div>{props.productPrice.toLocaleString("ko-KR")}</div>
+          <div>{props.productPrice?.toLocaleString("ko-KR")}</div>
         </div>
+        {props.productClick && (
+          <CommBtn
+            btnText="선택"
+            btnWidth="50px"
+            btnHeight="33px"
+            fontSize="17px"
+            fnClick={productClickBtnHandler}
+          />
+        )}
       </div>
     </div>
   );
