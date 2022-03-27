17ebeaa9799e3781156c847a5f4cbc6779723f43
yrkwon <moonctp24@gmail.com>
2022-03-27 14:57:50 +0900

    warning 제거

--- src/pages/account/accountFollowingPage.js
+++ src/pages/account/accountFollowingPage.js
@@ -98,136 +98,136 @@ const AcctFllwngPage = (props) => {
     );
   };
 
-  const DUMMY_DATA = [
-    {
-      id: 1,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164698562600101440.jpeg?gif=1&w=1280",
-      nick_name: "april_.12",
-      text: "instagram-april_.12",
-      userId: "user1",
-    },
-    {
-      id: 2,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/164517258818757910.jpg?gif=1&w=480&h=320&c=c",
-      nick_name: "Dazed_lily",
-      text: "나만의 작업실에서 좋아하는 것들을 만들고 있습니다.",
-      userId: "user2",
-    },
-    {
-      id: 3,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164760255716751290.jpeg?gif=1&w=1280",
-      nick_name: "sshinsy(세영)",
-      text: "Instagram @yayonine",
-      userId: "user3",
-    },
-    {
-      id: 4,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/159719442642625646.jpg?gif=1&w=1280&h=1280&c=c",
-      nick_name: "주아주아홈",
-      text: "화이트 아치 하우스 주아주아홈입니다.",
-      userId: "user4",
-    },
-    {
-      id: 1,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164698562600101440.jpeg?gif=1&w=1280",
-      nick_name: "april_.12",
-      text: "instagram-april_.12",
-      userId: "user5",
-    },
-    {
-      id: 2,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/164517258818757910.jpg?gif=1&w=480&h=320&c=c",
-      nick_name: "Dazed_lily",
-      text: "나만의 작업실에서 좋아하는 것들을 만들고 있습니다.",
-      userId: "user6",
-    },
-    {
-      id: 3,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164760255716751290.jpeg?gif=1&w=1280",
-      nick_name: "sshinsy(세영)",
-      text: "Instagram @yayonine",
-      userId: "user7",
-    },
-    {
-      id: 4,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/159719442642625646.jpg?gif=1&w=1280&h=1280&c=c",
-      nick_name: "주아주아홈",
-      text: "화이트 아치 하우스 주아주아홈입니다.",
-      userId: "user8",
-    },
-    {
-      id: 1,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164698562600101440.jpeg?gif=1&w=1280",
-      nick_name: "april_.12",
-      text: "instagram-april_.12",
-      userId: "user9",
-    },
-    {
-      id: 2,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/164517258818757910.jpg?gif=1&w=480&h=320&c=c",
-      nick_name: "Dazed_lily",
-      text: "나만의 작업실에서 좋아하는 것들을 만들고 있습니다.",
-      userId: "user10",
-    },
-    {
-      id: 3,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164760255716751290.jpeg?gif=1&w=1280",
-      nick_name: "sshinsy(세영)",
-      text: "Instagram @yayonine",
-      userId: "user11",
-    },
-    {
-      id: 4,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/159719442642625646.jpg?gif=1&w=1280&h=1280&c=c",
-      nick_name: "주아주아홈",
-      text: "화이트 아치 하우스 주아주아홈입니다.",
-      userId: "user12",
-    },
-    {
-      id: 1,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164698562600101440.jpeg?gif=1&w=1280",
-      nick_name: "april_.12",
-      text: "instagram-april_.12",
-      userId: "user13",
-    },
-    {
-      id: 2,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/164517258818757910.jpg?gif=1&w=480&h=320&c=c",
-      nick_name: "Dazed_lily",
-      text: "나만의 작업실에서 좋아하는 것들을 만들고 있습니다.",
-      userId: "user14",
-    },
-    {
-      id: 3,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164760255716751290.jpeg?gif=1&w=1280",
-      nick_name: "sshinsy(세영)",
-      text: "Instagram @yayonine",
-      userId: "user15",
-    },
-    {
-      id: 4,
-      profile_img:
-        "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/159719442642625646.jpg?gif=1&w=1280&h=1280&c=c",
-      nick_name: "주아주아홈",
-      text: "화이트 아치 하우스 주아주아홈입니다.",
-      userId: "user16",
-    },
-  ];
+  // const DUMMY_DATA = [
+  //   {
+  //     id: 1,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164698562600101440.jpeg?gif=1&w=1280",
+  //     nick_name: "april_.12",
+  //     text: "instagram-april_.12",
+  //     userId: "user1",
+  //   },
+  //   {
+  //     id: 2,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/164517258818757910.jpg?gif=1&w=480&h=320&c=c",
+  //     nick_name: "Dazed_lily",
+  //     text: "나만의 작업실에서 좋아하는 것들을 만들고 있습니다.",
+  //     userId: "user2",
+  //   },
+  //   {
+  //     id: 3,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164760255716751290.jpeg?gif=1&w=1280",
+  //     nick_name: "sshinsy(세영)",
+  //     text: "Instagram @yayonine",
+  //     userId: "user3",
+  //   },
+  //   {
+  //     id: 4,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/159719442642625646.jpg?gif=1&w=1280&h=1280&c=c",
+  //     nick_name: "주아주아홈",
+  //     text: "화이트 아치 하우스 주아주아홈입니다.",
+  //     userId: "user4",
+  //   },
+  //   {
+  //     id: 1,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164698562600101440.jpeg?gif=1&w=1280",
+  //     nick_name: "april_.12",
+  //     text: "instagram-april_.12",
+  //     userId: "user5",
+  //   },
+  //   {
+  //     id: 2,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/164517258818757910.jpg?gif=1&w=480&h=320&c=c",
+  //     nick_name: "Dazed_lily",
+  //     text: "나만의 작업실에서 좋아하는 것들을 만들고 있습니다.",
+  //     userId: "user6",
+  //   },
+  //   {
+  //     id: 3,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164760255716751290.jpeg?gif=1&w=1280",
+  //     nick_name: "sshinsy(세영)",
+  //     text: "Instagram @yayonine",
+  //     userId: "user7",
+  //   },
+  //   {
+  //     id: 4,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/159719442642625646.jpg?gif=1&w=1280&h=1280&c=c",
+  //     nick_name: "주아주아홈",
+  //     text: "화이트 아치 하우스 주아주아홈입니다.",
+  //     userId: "user8",
+  //   },
+  //   {
+  //     id: 1,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164698562600101440.jpeg?gif=1&w=1280",
+  //     nick_name: "april_.12",
+  //     text: "instagram-april_.12",
+  //     userId: "user9",
+  //   },
+  //   {
+  //     id: 2,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/164517258818757910.jpg?gif=1&w=480&h=320&c=c",
+  //     nick_name: "Dazed_lily",
+  //     text: "나만의 작업실에서 좋아하는 것들을 만들고 있습니다.",
+  //     userId: "user10",
+  //   },
+  //   {
+  //     id: 3,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164760255716751290.jpeg?gif=1&w=1280",
+  //     nick_name: "sshinsy(세영)",
+  //     text: "Instagram @yayonine",
+  //     userId: "user11",
+  //   },
+  //   {
+  //     id: 4,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/159719442642625646.jpg?gif=1&w=1280&h=1280&c=c",
+  //     nick_name: "주아주아홈",
+  //     text: "화이트 아치 하우스 주아주아홈입니다.",
+  //     userId: "user12",
+  //   },
+  //   {
+  //     id: 1,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164698562600101440.jpeg?gif=1&w=1280",
+  //     nick_name: "april_.12",
+  //     text: "instagram-april_.12",
+  //     userId: "user13",
+  //   },
+  //   {
+  //     id: 2,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/164517258818757910.jpg?gif=1&w=480&h=320&c=c",
+  //     nick_name: "Dazed_lily",
+  //     text: "나만의 작업실에서 좋아하는 것들을 만들고 있습니다.",
+  //     userId: "user14",
+  //   },
+  //   {
+  //     id: 3,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/164760255716751290.jpeg?gif=1&w=1280",
+  //     nick_name: "sshinsy(세영)",
+  //     text: "Instagram @yayonine",
+  //     userId: "user15",
+  //   },
+  //   {
+  //     id: 4,
+  //     profile_img:
+  //       "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/159719442642625646.jpg?gif=1&w=1280&h=1280&c=c",
+  //     nick_name: "주아주아홈",
+  //     text: "화이트 아치 하우스 주아주아홈입니다.",
+  //     userId: "user16",
+  //   },
+  // ];
   return (
     <div className={classes.layout}>
       <div className={classes.title}>
