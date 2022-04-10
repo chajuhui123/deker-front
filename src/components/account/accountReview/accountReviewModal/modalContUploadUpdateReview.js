aade981250ae4e81beaf6568526ced89a22074f5
Juhee <chajuhui123@naver.com>
2022-04-10 15:10:57 +0900

    fix(jhcha): 리뷰 작성 state 수정

--- src/components/account/accountReview/accountReviewModal/modalContUploadUpdateReview.js
+++ src/components/account/accountReview/accountReviewModal/modalContUploadUpdateReview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./modalContUploadUpdateReview.module.css";
 import InfoProduct from "./infoProduct";
 import StarRating from "./starRating";
@@ -6,7 +6,16 @@ import AddPhotoFile from "./addPhotoFile";
 import WritingReview from "./writingReview";
 
 function ModalContUploadUpdateReview({ isReviewed }) {
-  // 더미 데이터 추가 예정
+  const [rating, setRating] = useState(0); // 리뷰 별점
+  const [reviewContent, setReviewContent] = useState(""); // 리뷰 작성 내용
+
+  const handleRating = (rate) => setRating(rate);
+  const onChangeReviewContent = (event) => setReviewContent(event.target.value);
+
+  const handleAddReview = () => {
+    console.log(rating / 20, reviewContent);
+  };
+
   return (
     // 추후 Form type 으로 변경 예정
     <div className={classes.modalContReview}>
@@ -14,11 +23,16 @@ function ModalContUploadUpdateReview({ isReviewed }) {
         {!isReviewed && <p>리뷰 작성</p>}
         {isReviewed && <p>리뷰 수정</p>}
       </div>
-      <InfoProduct brandName={null} productName={null} productOption={null} />
-      <StarRating />
+      <InfoProduct brandName={"null"} productName={null} productOption={null} />
+      <StarRating rating={rating} handleRating={handleRating} />
       <AddPhotoFile />
-      <WritingReview />
-      <button className={classes.btn}>등록</button>
+      <WritingReview
+        reviewContent={reviewContent}
+        onChangeReviewContent={onChangeReviewContent}
+      />
+      <button className={classes.btn} onClick={handleAddReview}>
+        등록
+      </button>
     </div>
   );
 }
