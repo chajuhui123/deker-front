9009546a48543053c50f15d451aa31158f652607
Juhee <chajuhui123@naver.com>
2022-06-12 17:27:46 +0900

    feat: 상품 등록 페이지 레이아웃 변경 진행

--- src/components/shop/registrationProduct/info/registrationProductInfo.js
+++ src/components/shop/registrationProduct/info/registrationProductInfo.js
@@ -1,30 +1,39 @@
 import React from "react";
+import classes from "./registrationProductInfo.module.css";
 import CommInput from "components/common/commInput";
 
 function RegistrationProductInfo({ handleUploadImage }) {
   return (
     <>
       <h3>기본 정보</h3>
-      {/* 1. 상품 대표 이미지  */}
-      <input type="file" accept="img/*" onChange={handleUploadImage} />
-      <div
-        className="previewImg"
-        style={{
-          width: "200px",
-          height: "200px",
-          backgroundColor: "#f0f0f0",
-          backgroundSize: "cover",
-        }}
-      />
-      {/* 2.카테고리 */}
-      {/* 셀렉트 박스 추가 퍼블리싱 필요 */}
-      <CommInput placeholder="옵션 카테고리" />
-      {/* 3. 상품명  */}
-      <CommInput placeholder="상품명" />
-      {/* 4. 기본 가격  */}
-      <CommInput type="number" placeholder="기본 가격" />
-      {/* 5. 상품 판매 갯수  */}
-      <CommInput type="number" placeholder="상품 판매 갯수" />
+      <div className={classes.flex}>
+        <div className={classes.imgDiv}>
+          {/* 1. 상품 대표 이미지  */}
+          <input
+            className={classes.imgInput}
+            id="represent-img"
+            type="file"
+            accept="img/*"
+            onChange={handleUploadImage}
+          />
+          <label className={classes.imgLabel} for="represent-img" />
+        </div>
+        <div className={classes.inputDiv}>
+          {/* 2.카테고리 */}
+          {/* 셀렉트 박스 추가 퍼블리싱 필요 */}
+          <CommInput placeholder="옵션 카테고리" />
+          {/* 3. 상품명  */}
+          <CommInput placeholder="상품명" />
+          {/* 4. 기본 가격  */}
+          <CommInput type="number" placeholder="기본 가격" />
+          {/* 5. 상품 판매 갯수  */}
+          <CommInput
+            type="number"
+            placeholder="상품 판매 갯수"
+            style={{ margin: "0px" }}
+          />
+        </div>
+      </div>
     </>
   );
 }
