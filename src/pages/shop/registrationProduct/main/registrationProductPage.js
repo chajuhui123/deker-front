c383045a3630d1d079d6e48a2804a705b732fde5
chajuhui123 <chajuhui123@naver.com>
2022-06-26 14:50:51 +0900

    feat: 상품 기본 정보 이미지 미리보기 기능 추가

--- src/pages/shop/registrationProduct/main/registrationProductPage.js
+++ src/pages/shop/registrationProduct/main/registrationProductPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import classes from "./registrationProductPage.module.css";
 import CommBtn from "components/common/commBtn";
 import RegistrationProductInfo from "components/shop/registrationProduct/info/registrationProductInfo";
@@ -6,8 +6,6 @@ import RegistrationProductDetail from "components/shop/registrationProduct/detai
 import RegistrationProductCategory from "components/shop/registrationProduct/category/registrationProductCategory";
 
 const RegistrationProductPage = () => {
-  // const _ = require("lodash");
-
   const [productRegistrationState, setProductRegistrationState] = useState({
     productImg: "",
     productInfo: {
@@ -28,71 +26,6 @@ const RegistrationProductPage = () => {
     { label: "사무용품", value: "" },
     { label: "데코", value: "" },
   ];
-
-  // const dummy_productOptions = [
-  //   {
-  //     category: "색상",
-  //     options: [
-  //       { option: "black", addPrice: 0 },
-  //       { option: "white", addPrice: 1000 },
-  //       { option: "navy", addPrice: 2000 },
-  //     ],
-  //   },
-  // ];
-
-  const [productFiles, setProductFiles] = useState();
-
-  // 상품 기본 정보
-
-  const previewImage = () => {
-    if (!productFiles) return;
-
-    const imgElement = document.querySelector(".previewImg");
-    const reader = new FileReader();
-
-    reader.onload = () => {
-      imgElement.style.backgroundImage = `url(${reader.result})`;
-    };
-    reader.readAsDataURL(productFiles[0]);
-  };
-
-  // 상품 카테고리
-
-  // const handleAddCategory = () => {
-  //   if (productOption.length >= 2) return;
-  //   setProductOption((prev) => [
-  //     ...prev,
-  //     { category: "추후 props 값으로 수정", options: [] },
-  //   ]);
-  // };
-
-  // const handleDeleteCategory = (categoryIndex) => {
-  //   const copiedProductOption = _.cloneDeep(productOption);
-  //   categoryIndex === 0
-  //     ? copiedProductOption.shift()
-  //     : copiedProductOption.pop();
-  //   setProductOption(copiedProductOption);
-  // };
-
-  // const handleAddCategoryOption = (categoryIndex) => {
-  //   const copiedProductOption = _.cloneDeep(productOption);
-  //   copiedProductOption[categoryIndex].options.push({
-  //     option: "",
-  //     addPrice: 0,
-  //   });
-  //   setProductOption(copiedProductOption);
-  // };
-
-  // const handleDeleteCategoryOption = (categoryIndex) => {
-  //   const copiedProductOption = _.cloneDeep(productOption);
-  //   copiedProductOption[categoryIndex].options.pop();
-  //   setProductOption(copiedProductOption);
-  // };
-
-  useEffect(() => {
-    previewImage();
-    return () => previewImage();
-  });
 
   return (
     <div>
